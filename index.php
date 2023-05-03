36467f370b7dbef5fc8fa26bb61d2a9abcb684ab
jennni98 <132424354+jennni98@users.noreply.github.com>
2023-05-02 23:26:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ include("marco-right.php");
    <div  style="width: 100%;">     
     <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
        <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
-        	<h1>¿Quién es Alan Ramirez?</h1>
+        	<h1>¿Quién es Jennifer Ortega ?</h1>
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  Alan Ramirez<br>
+                Posteado por:  Jennifer Ortega <br>
                 Fecha: 2023-04-02 <br>
           </div>
         </div>
@@ -24,9 +24,11 @@ include("marco-right.php");
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">¡Hola! Me complace presentarme, soy Ramírez Mera <b> Alan Ricardo </b>, estudiante de Ingeniería en Tecnologías de la Información en la Universidad Técnica Luis Vargas Torres de Esmeraldas. </p>
-                   <p class="eys-parrafo">Me apasiona el mundo de la tecnología y todo lo que tenga que ver con la informática, la programación y el desarrollo de software.</p>
-                   <p class="eys-parrafo">Mi objetivo a largo plazo es convertirme en un profesional exitoso y respetado en el campo de la tecnología, y contribuir al desarrollo de soluciones innovadoras que puedan mejorar la vida de las personas y la sociedad en general. Para lograrlo, estoy constantemente en busca de nuevas oportunidades de aprendizaje y crecimiento profesional, además de mantenerme actualizado sobre las últimas tendencias y avances en la industria.</p>
+                   <p class="eys-parrafo">¡Hola! Me complace presentarme, soy Alvarez Ortega <b> Jennifer </b>, soy una estudiante de Ingeniería en tecnología de la información y comunicación con una gran carrera en la que estudio en la Universidad Técnica Luis Vargas torres de esmeraldas. </p>
+                   <p class="eys-parrafo">Desde que inicie mi carrera me gusta mucho la programación como va evolucionando a través de los tiempos me interesa mucho el diseño de páginas web.</p>
+                   <p class="eys-parrafo">Me interesa mucho en el diseño de paginas por que puedo ser creativa con las cosas que le aplico y cada diseño que se hace es diferente ya que hay compañeros en la que me piden que les ayude en su diseño puedo agregarle mi estilo propio.</p>
+                   <p class="eys-parrafo">Además de eso me gusta mucho lo que es grafica y multimedia ya que puedo involúcrame en el funcionamiento de las aplicaciones y sus diferentes programas que existen en el mundo del internet.</p>
+                   <p class="eys-parrafo">Me emociona que la tecnología hoy en día esta desarrollándose y que es muy versátil para emprender y conseguir trabajo, aunque también hay mucho desempleo en la ciudad. </p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -34,24 +36,23 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en la  Unidad Educativa Fiscal "Fausto Molina", donde me gradue como Bachiller en Ciencias. Durante mis estudios secundarios,me apasioné por la física y las matemáticas, lo que me impulsó a seguir una carrera en el campo de la tecnología y la ciencia.</p>
-                   <p class="eys-parrafo">Mi formación en ciencias y mi pasión por la tecnología me llevaron a decidir estudiar ingeniería en tecnologías de la información en la Universidad Técnica Luis Vargas Torres de Esmeraldas. Allí, he ampliado mis conocimientos en programación y desarrollo de software, aprendiendo lenguajes de programación como C ++ o PHP.</p>
-<p class="eys-parrafo"> Mi objetivo como profesional de la informática es continuar desarrollando habilidades en el campo de la tecnología y la innovación, trabajando en proyectos que impacten positivamente en la sociedad y en el mundo empresarial. Además, como persona, aspiro a ser un ciudadano comprometido y responsable, siempre buscando oportunidades para contribuir al bienestar de mi comunidad.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios lo realicé en la Unidad Educativa Muisne, me gradué de Química durante mis estudios descubrí una amplia variedad de habilidades que me ayudaron en algunas actividades dentro de la carrera.</p>
+                   <p class="eys-parrafo">Además de mi carrera participe en varias actividades de organizaban en mi cantón entre a un curso de enfermería en la cual me gradué con gran éxito y en la cual complementaria con mis estudios que estaba siguiendo en el colegio.</p>
+
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
     
 
 
-                   <p  class="eys-parrafo">Soy una persona apasionada por la tecnología y me gusta estar al día con las últimas tendencias en el mundo de la informática. .</p> 
-                   <p  class="eys-parrafo">Tengo habilidades en sistemas informaticos y hadware, lo que me permite ser más eficiente en mi trabajo y en el uso de herramientas digitales en mi día a día.</p>
-                   <p  class="eys-parrafo">Además, me gusta aprender sobre seguridad informática y cómo proteger mi información personal en línea. Estoy en constante aprendizaje y siempre busco mejorar mis habilidades para poder enfrentar nuevos retos en el campo de la tecnología.</p>
+                   <p  class="eys-parrafo">Tengo experiencia en desarrollo web utilizando tecnología como HTML y CSS, además de eso trato de interesarme   en otras cosas como es aprender sobre la materia de base de datos ya que es muy interesante demás de la programación para ser versátil al momento de postular a un empleo.</p> 
+                  
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy una persona activa y me encanta disfrutar de mi tiempo libre realizando actividades que me permitan mantenerme en forma y relajado. Uno de mis hobbies favoritos es el deporte, en especial el fútbol y el baloncesto. Me gusta jugar con amigos los fines de semana y ver partidos en la televisión. </p>
+                   <p   class="eys-parrafo">Es mis ratos libres me gusta escuchar música además de leer y ver novelas turcas, me gusta quedarme en casa y entretenerme un rato en las redes sociales. </p>
        
 
                    
@@ -81,9 +82,9 @@ include("marco-left.php")
  
 <script >  
 
- document.title="Alan Ramirez"; 
-document.getElementById("foto").src="./images/RamirezAlan.jpeg";
-document.getElementById("elnombre").innerHTML="Alan Ramirez";
+ document.title="Jennifer Ortega"; 
+document.getElementById("foto").src="./images/OrtegaJennifer.jpeg";
+document.getElementById("elnombre").innerHTML="Jennifer Ortega ";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 

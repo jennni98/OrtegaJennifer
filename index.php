fae98ac2252ab332b7e6d10030f7e68ca312b8ef
jennni98 <132424354+jennni98@users.noreply.github.com>
2023-05-02 23:19:59 -0500

    Add files via upload
    basicos

---
+++ index.php
@@ -0,0 +1,95 @@
+
+<?php
+include("head.php");
+?>
+
+
+<?php
+include("marco-right.php");
+?>
+   
+ <div style="float:left; width:60%; margin:60px auto; padding:1vw">
+   <div  style="width: 100%;">     
+    <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
+       <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
+        	<h1>¿Quién es Alan Ramirez?</h1>
+       		 <hr>
+             
+          <div id="posteo" style="text-align: left; font-size:0.5vw;">
+                Posteado por:  Alan Ramirez<br>
+                Fecha: 2023-04-02 <br>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div style="width: 100%; height:100%;" >
+           <div  style="text-align:justify; font-size: 20px;">
+                   <p class="eys-parrafo">¡Hola! Me complace presentarme, soy Ramírez Mera <b> Alan Ricardo </b>, estudiante de Ingeniería en Tecnologías de la Información en la Universidad Técnica Luis Vargas Torres de Esmeraldas. </p>
+                   <p class="eys-parrafo">Me apasiona el mundo de la tecnología y todo lo que tenga que ver con la informática, la programación y el desarrollo de software.</p>
+                   <p class="eys-parrafo">Mi objetivo a largo plazo es convertirme en un profesional exitoso y respetado en el campo de la tecnología, y contribuir al desarrollo de soluciones innovadoras que puedan mejorar la vida de las personas y la sociedad en general. Para lograrlo, estoy constantemente en busca de nuevas oportunidades de aprendizaje y crecimiento profesional, además de mantenerme actualizado sobre las últimas tendencias y avances en la industria.</p>
+
+   <div id="blog-seccion2"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
+    
+
+
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en la  Unidad Educativa Fiscal "Fausto Molina", donde me gradue como Bachiller en Ciencias. Durante mis estudios secundarios,me apasioné por la física y las matemáticas, lo que me impulsó a seguir una carrera en el campo de la tecnología y la ciencia.</p>
+                   <p class="eys-parrafo">Mi formación en ciencias y mi pasión por la tecnología me llevaron a decidir estudiar ingeniería en tecnologías de la información en la Universidad Técnica Luis Vargas Torres de Esmeraldas. Allí, he ampliado mis conocimientos en programación y desarrollo de software, aprendiendo lenguajes de programación como C ++ o PHP.</p>
+<p class="eys-parrafo"> Mi objetivo como profesional de la informática es continuar desarrollando habilidades en el campo de la tecnología y la innovación, trabajando en proyectos que impacten positivamente en la sociedad y en el mundo empresarial. Además, como persona, aspiro a ser un ciudadano comprometido y responsable, siempre buscando oportunidades para contribuir al bienestar de mi comunidad.</p>
+   <div id="blog-seccion3"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
+    
+
+
+                   <p  class="eys-parrafo">Soy una persona apasionada por la tecnología y me gusta estar al día con las últimas tendencias en el mundo de la informática. .</p> 
+                   <p  class="eys-parrafo">Tengo habilidades en sistemas informaticos y hadware, lo que me permite ser más eficiente en mi trabajo y en el uso de herramientas digitales en mi día a día.</p>
+                   <p  class="eys-parrafo">Además, me gusta aprender sobre seguridad informática y cómo proteger mi información personal en línea. Estoy en constante aprendizaje y siempre busco mejorar mis habilidades para poder enfrentar nuevos retos en el campo de la tecnología.</p>
+   <div id="blog-seccion4"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
+    
+
+                   <p   class="eys-parrafo">Soy una persona activa y me encanta disfrutar de mi tiempo libre realizando actividades que me permitan mantenerme en forma y relajado. Uno de mis hobbies favoritos es el deporte, en especial el fútbol y el baloncesto. Me gusta jugar con amigos los fines de semana y ver partidos en la televisión. </p>
+       
+
+                   
+
+               <hr>
+               <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
+                  <div style="width: 40%;  float: left;">
+                   <a href="#">&#171;   Previo</a>
+                  </div>
+
+                  <div style="width: 40%; float: right; text-align: right;">
+                   <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
+                  </div>
+
+                </div>
+           </div>
+  </div> 
+
+  </div>
+
+
+
+
+<?php
+include("marco-left.php")
+?>
+ 
+<script >  
+
+ document.title="Alan Ramirez"; 
+document.getElementById("foto").src="./images/RamirezAlan.jpeg";
+document.getElementById("elnombre").innerHTML="Alan Ramirez";
+
+ document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
+
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='https://congresoutlvte.org/CabezasNicole/'>Nicole Cabezas</a></p><a href='https://congresoutlvte.org/TapiaBarbara/'>Barbara Tapia</a></p> <a href='https://congresoutlvte.org/RamirezAlan/'>Alan Ramirez</a></p><a href='https://congresoutlvte.org/FrancoIsaac/'>Isaac Franco</a></p> ";
+ </script>
+ 
+<?php
+include("foot.php")
+?>
